b5349cf26185208698c5025a883a19707c4e059b
Stefano <stefe68@gmail.com>
2023-12-01 17:32:44 +0100

    Add carousel component and css style

---
+++ src/assets/db/data.js
@@ -0,0 +1,25 @@
+const carouselData = [
+	{
+		text: "Prima immagine",
+		image: "img1.jpg"
+	},
+
+	{
+		text: "Seconda immagine",
+		image: "img2.jpg"
+	},
+	{
+		text: "Terza immagine",
+		image: "img3.jpg"
+	},
+	{
+		text: "Quarta immagine",
+		image: "img4.jpg"
+	},
+	{
+		text: "Quinta immagine",
+		image: "img5.jpg"
+	},
+];
+
+export default carouselData;
--- src/components/TheCarousel.jsx
+++ src/components/TheCarousel.jsx
@@ -1,10 +1,34 @@
 import {useState} from "react";
 import style from "../css/modules/Carousel.module.css";
+import carouselData from "../assets/db/data.js";
 
 function TheCarousel() {
+	const [currentSlide, setCurrentSlide] = useState(0);
+
+	const nextSlide = () =>
+		setCurrentSlide((prev) => (prev + 1) % carouselData.length);
+
+	const prevSlide = () =>
+		setCurrentSlide(
+			(prev) => (prev - 1 + carouselData.length) % carouselData.length,
+		);
+
 	return (
-		<div className="container">
-			<i class="fa-solid fa-user"></i>;
+		<div className={style.carousel}>
+			<div className={style.image}>
+				<img src={`/${carouselData[currentSlide].image}`} alt="Slide" />
+				<button
+					className={`${style.btnCarousel} ${style.btnPrev}`}
+					onClick={prevSlide}>
+					Prev
+				</button>
+				<button
+					className={`${style.btnCarousel} ${style.btnNext}`}
+					onClick={nextSlide}>
+					Next
+				</button>
+				<p className={style.text}>{carouselData[currentSlide].text}</p>
+			</div>
 		</div>
 	);
 }
